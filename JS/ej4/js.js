aeb7fc0eee8f5dd3d86320081057b63c9e88baad
Carlos A. S <as.carlos.dev@gmail.com>
2024-04-12 10:54:53 +0200

    Cambio a rutas relativas

---
+++ JS/ej4/js.js
@@ -0,0 +1,17 @@
+let contenido = document.getElementById("contenido");
+
+
+function printFecha() {
+
+
+    let fecha = new Date;
+    
+
+    contenido.innerHTML = fecha;    
+    
+    setTimeout(printFecha, 1000);
+}
+
+printFecha();
+
+
